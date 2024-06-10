53e876fd6d0355ccf85840fc4a479ad88c6ae920
kweku-xvi <nksarps@gmail.com>
2024-06-10 21:08:34 +0000

    views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from .forms import UserRegistrationForm
+from .models import User
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
+
+def sign_up_view(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'accounts/register.html', {'form':form})
