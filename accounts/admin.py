c503e7ce610285cf449d2a1c8257b27b428f1f6c
kweku-xvi <nksarps@gmail.com>
2024-06-10 21:07:51 +0000

    added users to admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
+from .models import User
 from django.contrib import admin
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'username', 'email', 'is_verified')
+
+
+admin.site.register(User, UserAdmin)
