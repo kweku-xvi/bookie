bee9c046eb86310ec18e28ec986606e51d002703
kweku-xvi <nksarps@gmail.com>
2024-06-11 14:09:24 +0000

    refactor urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('signup/', views.sign_up_view, name='signup'),
     path('verify-user/', views.verify_user_view, name='verify_user'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
