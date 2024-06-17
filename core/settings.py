8f8d9f8e5ba2bbf4994d82749e502127a10479c0
kweku-xvi <nksarps@gmail.com>
2024-06-17 15:57:53 +0000

    core

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'accounts',
     'crispy_bootstrap4',
     'crispy_forms',
+    'programs',
+    'tickets',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- core/urls.py
+++ core/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('events/', include('programs.urls')),
 ]
 
 if settings.DEBUG:
