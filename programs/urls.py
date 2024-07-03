a5254abfd5adea1cec3ecb1c83df6d5f39953985
kweku-xvi <nksarps@gmail.com>
2024-07-03 13:15:17 +0000

    bookie

--- programs/urls.py
+++ programs/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('create-event/', views.create_event_view, name='create_event'),
-    path('<str:event_id>/', views.events_info_view, name='events_info'),
+    path('org/<str:id>/', views.organizer_profile_view, name='event_organizer'),
+    path('<str:event_id>/', views.events_info_view, name='events_info'),   
 ]
--- programs/views.py
+++ programs/views.py
@@ -1,5 +1,6 @@
 from .forms import EventsForm
 from .models import Event
+from accounts.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
@@ -27,7 +28,10 @@ def create_event_view(request):
 
 
 def events_info_view(request, event_id:str):
-    event = Event.objects.get(id=event_id)
+    if Event.objects.filter(id=event_id).exists:
+        event = Event.objects.get(id=event_id)
+    else:
+        return HttpResponse('<h1>Event does not exist</h1>')
 
     context = {
         'title':event.name,
@@ -35,3 +39,14 @@ def events_info_view(request, event_id:str):
     }
 
     return render(request, 'programs/events_info.html', context)
+
+
+def organizer_profile_view(request, id:str):
+    user = User.objects.get(id=id)
+
+    context = {
+        'title':user.username,
+        'user':user
+    }
+
+    return render(request, 'programs/organizer_profile.html', context)
