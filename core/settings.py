e3e374d21631b1b00fef48630514eb91df361ba6
kweku-xvi <nksarps@gmail.com>
2024-07-21 13:58:41 +0000

    bookie

--- core/settings.py
+++ core/settings.py
@@ -160,6 +160,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/static/media')
 MEDIA_URL = '/media/'
 
