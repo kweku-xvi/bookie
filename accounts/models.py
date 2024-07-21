b59f6265875e955d502f56207540d02001ee3f42
kweku-xvi <nksarps@gmail.com>
2024-07-21 11:07:45 +0000

    bookie

--- accounts/models.py
+++ accounts/models.py
@@ -18,6 +18,7 @@ class MyUserManager(BaseUserManager):
     def create_superuser(self,email,password=None,**extra_fields):
         extra_fields.setdefault("is_staff",True)
         extra_fields.setdefault("is_superuser",True)
+        extra_fields.setdefault("is_verified", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("SuperUser must have is_staff=True"))
