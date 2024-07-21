963812c975e08cce08f8564ec1c2dda3801f1417
kweku-xvi <nksarps@gmail.com>
2024-07-21 10:50:20 +0000

    bookie

--- core/settings.py
+++ core/settings.py
@@ -142,6 +142,9 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Default primary key field type
