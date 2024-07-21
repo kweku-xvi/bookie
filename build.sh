d955c9f91aa0ada973b44594b1e3a1a7fdd8de58
kweku-xvi <nksarps@gmail.com>
2024-07-21 15:29:31 +0000

    bookie

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
+python manage.py makemigrations
 python manage.py migrate
 
 # Create a superuser if one does not already exist
